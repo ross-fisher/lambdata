452dc94ee00d83568cfd3620292d5f578b5ac3e4
Ross Fisher <rcfisher1097@gmail.com>
2019-09-30 05:21:49 -0500

    added module lambdata_me

---
+++ lambdata_me/__init__.py
@@ -0,0 +1,6 @@
+# pipenv shell
+import pandas as pd
+import numpy as np
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
---
+++ lambdata_me/df_utils.py
@@ -0,0 +1,10 @@
+"""
+About: Utility Functions for working with Dataframes
+
+"""
+
+import pandas as pd
+
+test_df = pd.DataFrame([1,2,3])
+
+
