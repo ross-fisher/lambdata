d4a7e8a954c4f897c366ff96d75c03e8c5791526
Ross Fisher <rcfisher1097@gmail.com>
2019-10-02 05:44:58 -0500

    aoetnuh

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
             name="lambdata-md",
-            version="0.1.2",
+            version="0.1.3",
             author="ross-fisher",
             description="a collection of data science helper functions",
             long_description = LONG_DESCRIPTION,
